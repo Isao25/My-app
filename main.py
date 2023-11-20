1b3907efa99aa8decd2dedfe3dfd2266f0d71aaa
Isao25 <shamir.mantilla@unmsm.edu.pe>
2023-11-20 01:17:24 -0500

    Termine el metodo de evalución

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 from flask import Flask, render_template, request, session, redirect
 import pandas as pd
 import os
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 app = Flask(__name__)
 app.secret_key = os.urandom(24)
+
+def calcular_similitud(enfermedad, sintomas_usuario):
+    sintomas_enfermedad = set(symptom for symptom in enfermedad.iloc[1:].dropna().tolist() if symptom)
+    sintomas_usuario = set(sintomas_usuario)
+
+    if not sintomas_enfermedad:
+        return None  # Evitar divisiones por cero
+
+    # Calcular la similitud entre los conjuntos de síntomas
+    similitud = len(sintomas_enfermedad.intersection(sintomas_usuario)) / len(sintomas_enfermedad)
+
+    return similitud
+
+def obtener_diccionario_similitud(df, sintomas_usuario):
+    diccionario_similitud = {}
+
+    for i, enfermedad in df.iterrows():
+        similitud = calcular_similitud(enfermedad, sintomas_usuario)
+
+        if similitud is not None and similitud > 0:
+            diccionario_similitud[enfermedad['Disease']] = round(similitud * 100,2)  # Convertir a porcentaje
+
+    return diccionario_similitud
 
 @app.route('/')
 def home():
@@ -49,6 +75,37 @@ def eliminar_sintoma():
 
     return redirect('/evaluacion')
 
+@app.route('/evaluacion/analizarSintomas')
+def analizar_sintoma():
+    enfermedades = pd.read_csv('static/csv/enfermedadSintomas.csv')
+    sintomas_agregados = session.get('sintomas_agregados', [])
+    resultado = obtener_diccionario_similitud(enfermedades, sintomas_agregados)
+
+    # Crear un DataFrame a partir del diccionario
+    df_resultado = pd.DataFrame(list(resultado.items()), columns=['Enfermedad', 'Similitud (%)'])
+
+    # Convertir DataFrame a HTML
+    tabla_html = df_resultado.to_html(index=False, classes='table table-striped')
+
+    # Crear un gráfico estadístico
+    plt.bar(df_resultado['Enfermedad'], df_resultado['Similitud (%)'])
+    plt.xlabel('Enfermedad')
+    plt.ylabel('Similitud (%)')
+    plt.title('Similitud de enfermedades con los síntomas ingresados')
+    plt.xticks(rotation=45, ha='right')
+
+    # Guardar el gráfico en un objeto BytesIO
+    img_buf = BytesIO()
+    plt.savefig(img_buf, format='png', bbox_inches='tight')
+    img_buf.seek(0)
+    
+    # Convertir la imagen a base64 para mostrarla en el HTML
+    img_base64 = base64.b64encode(img_buf.read()).decode('utf-8')
+    
+    # Cerrar la figura de matplotlib
+    plt.close()
+
+    return render_template('analizarSintomas.html', tabla_html=tabla_html, img_base64=img_base64)
 
 
 @app.route('/about')
